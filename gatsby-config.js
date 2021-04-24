f2a07362f5fd59140eff5d3689a2bfe3c690dcbc
Pavel Volynets <pavel.volynets@gmail.com>
2021-04-24 09:50:07 +0300

    lazyload images

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,5 +60,21 @@ module.exports = {
         host: process.env.GATSBY_CONTENTFUL_HOST,
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images-contentful`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
   ],
 };
