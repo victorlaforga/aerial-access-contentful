05891bcfa741d7c97cf26d978dcc21a11bb6ac96
Victor Laforga <victorlaforga@hotmail.com>
2021-08-17 16:06:33 +0200

    add small fixes

--- src/components/projecten/project.js
+++ src/components/projecten/project.js
@@ -23,11 +23,14 @@ const Project = (props) => {
           <h2>{title}</h2>
           <p>{shortDescriptionContent}</p>
           <br />
+          <p className="thin">{longDescriptionContent}</p>
+          <br />
           <a className="link_contact" href="/contact/">Wij informeren u graag over de mogelijkheden. Neem daarom geheel vrijblijvend contact met ons op. </a>
-          {imageSrc ? <img src={imageSrc} /> : <></>}
+          
         </div>
         <div className='project-block--text project-element'>
-          <p>{longDescriptionContent}</p>
+          {/* <p>{longDescriptionContent}</p> */}
+          {imageSrc ? <img src={imageSrc} /> : <></>}
         </div>
         <div className='project-block--work project-element'>
           {activities && activities.length ? (
--- src/components/systems/producten/collection-new.js
+++ src/components/systems/producten/collection-new.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import '../../../styles/styles.scss';
+import Carousel, { Dots } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
+import imageOne from "../../../images/slider/abs-logo.png"
+import imageTwo from "../../../images/slider/sep.png"
+import imageThree from "../../../images/slider/xs-logo.png"
+import imageFour from "../../../images/slider/skylotec.png"
+import imageFive from "../../../images/slider/harken-industrial_logo.png";
 
 const CollectionNew = (props) => {
   const { categories } = props;
 
   return  (
-    <section className='product-collections'>
+    <section className="container">
+
+      <h1>Categorieen</h1>
+      <div className='product-collections'>
       <a className="categorie_container" href="/">
       <div>
       <h1>Rail systeem</h1>
@@ -38,9 +49,23 @@ const CollectionNew = (props) => {
       <img src="//images.ctfassets.net/8cxl6mpst1xl/3cWv9g6BM83G49kqegbDn8/760704ef3f14c2b93b4be055ef8481cf/railsystem.png" alt=""/>
       </div>
       </a>
+      </div>
+      <Carousel slidesPerPage={2}
+      slidesPerScroll={2}
+      animationSpeed={1500}
+      autoPlay={2000}
+      offset={50}
+      itemWidth={350}
+      clickToChange
+      centered>
+
+        <img src={imageOne} />
+        <img src={imageTwo} />
+        <img src={imageThree} />
+        <img src={imageFour} />
+        <img src={imageFive} />
+      </Carousel>
     </section>
-  
-   
   );
 };
 
